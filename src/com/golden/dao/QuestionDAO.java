1c189ba36cdc1b1dffa5b66bea99f94e14bf1ede
Missaoui Chedy <missaoui.chedy@gmail.com>
2013-05-08 19:18:32 +0100

    Setted Up build.xml     Added Database Deployment in build script.

--- src/com/golden/dao/QuestionDAO.java
+++ src/com/golden/dao/QuestionDAO.java
@@ -1,7 +1,10 @@
 package com.golden.dao;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.golden.entities.Question;
-public interface QuestionDAO {
-    List<Question> findAll();
+public class QuestionDAO {
+    public List<Question> findAll(){
+    	return new ArrayList<Question>();
+    }
 }
